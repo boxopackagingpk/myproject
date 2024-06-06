a46f1ec02282433bc834e51b1e8481378b2761ed
boxopackagingpk <160975234+boxopackagingpk@users.noreply.github.com>
2024-06-07 03:18:46 +0500

    Update project.py

--- project.py
+++ project.py
@@ -39,6 +39,31 @@ schema XML microsoft
 ns0 = "http://www.w3.org/2005/Atom"
 ns1 = "http://schemas.microsoft.com/ado/2007/08/dataservices/metadata"
 ns2 = "http://schemas.microsoft.com/ado/2007/08/dataservices"
+schema XML microsoft 
+ns0 = "http://www.w3.org/2005/Atom"
+ns1 = "http://schemas.microsoft.com/ado/2007/08/dataservices/metadata"
+ns2 = "http://schemas.microsoft.com/ado/2007/08/dataservices"
+
+Create csv file 
+csv_file = open('file_name_csv.csv', 'w', newline = '', encoding='ansi')
+csvwriter = csv.writer(csv_file)
+
+col_names = ['Col_1', 'Col_2', 'Col_3', 'Col_n']
+csvwriter.writerow(col_names)
+
+field_tag = ['dado_1', 'dado_2', 'dado_3', 'dado_n']
+
+
+for member in root:
+    if member.tag == '{' + ns0 + '}entry':
+        for element in member:
+            if element.tag == '{' + ns0 + '}content':
+                data_line = []
+
+                for field in element[0]:            
+                    for count in range(0, len(field_tag)):
+                        if field.tag == '{' + ns2 + '}' + field_tag[count]:
+                            data_line.append(field.text)
 
 Create csv file 
 csv_file = open('file_name_csv.csv', 'w', newline = '', encoding='ansi')
