c463e0e81a49f1f63925541bdf1b187537fd04c5
boxopackagingpk <160975234+boxopackagingpk@users.noreply.github.com>
2025-01-20 23:27:58 +0500

    Update project.py

--- project.py
+++ project.py
@@ -54,6 +54,29 @@ for member in root:
                         if field.tag == '{' + ns2 + '}' + field_tag[count]:
                             data_line.append(field.text)
 
+response = requests.get("your_url", auth=HttpNtlmAuth('xxxx\\username','password'))
+tree =  ET.ElementTree(ET.fromstring(response.content))
+tree.write('file_name_xml.xml')
+root = tree.getroot()
+
+schema XML microsoft 
+ns0 = "http://www.w3.org/2005/Atom"
+ns1 = "http://schemas.microsoft.com/ado/2007/08/dataservices/metadata"
+ns2 = "http://schemas.microsoft.com/ado/2007/08/dataservices"
+schema XML microsoft 
+ns0 = "http://www.w3.org/2005/Atom"
+ns1 = "http://schemas.microsoft.com/ado/2007/08/dataservices/metadata"
+ns2 = "http://schemas.microsoft.com/ado/2007/08/dataservices"
+
+Create csv file 
+csv_file = open('file_name_csv.csv', 'w', newline = '', encoding='ansi')
+csvwriter = csv.writer(csv_file)
+
+col_names = ['Col_1', 'Col_2', 'Col_3', 'Col_n']
+csvwriter.writerow(col_names)
+
+field_tag = ['dado_1', 'dado_2', 'dado_3', 'dado_n']
+
 Create csv file 
 csv_file = open('file_name_csv.csv', 'w', newline = '', encoding='ansi')
 csvwriter = csv.writer(csv_file)
